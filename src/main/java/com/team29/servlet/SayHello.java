762f5ac08141dff366c500e30782a8c8e83caec3
马国棋 <240976371chess@gmail.com>
2023-07-17 16:32:47 +0800

    新建仓库

--- src/main/java/com/team29/servlet/SayHello.java
+++ src/main/java/com/team29/servlet/SayHello.java
@@ -1,2 +1,14 @@
-package com.team29.servlet;public class SayHello {
+package com.team29.servlet;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SayHello {
+
+    @RequestMapping("/sayhello")
+    public String getUserList() {
+        return "创建成功！";
+    }
+
 }
--- src/test/java/com/team29/BmSystemApplicationTests.java
+++ src/test/java/com/team29/BmSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BmSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

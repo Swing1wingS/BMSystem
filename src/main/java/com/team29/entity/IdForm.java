89a3a94b5c1037778c5a797da83685ba8b35913d
马国棋 <240976371chess@gmail.com>
2023-07-25 20:17:52 +0800

    完善登录功能

---
+++ src/main/java/com/team29/entity/IdForm.java
@@ -0,0 +1,13 @@
+package com.team29.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IdForm {
+    private Integer type;
+    private String id;
+}
--- src/main/java/com/team29/service/impl/LoginServiceImpl.java
+++ src/main/java/com/team29/service/impl/LoginServiceImpl.java
@@ -1,11 +1,9 @@
 package com.team29.service.impl;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.team29.dao.AdminDao;
 import com.team29.dao.UserDao;
-import com.team29.entity.Admin;
-import com.team29.entity.LoginForm;
-import com.team29.entity.Result;
-import com.team29.entity.User;
+import com.team29.entity.*;
 import com.team29.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,20 +20,23 @@ public class LoginServiceImpl implements LoginService {
     @Override
     public Result login(LoginForm loginForm)
     {
+        IdForm idForm;
         switch (loginForm.getType()) {
             case 0 :
                 User user = userDao.findUserByName(loginForm.getName());
                 if (user == null) {
                     return Result.fail("该用户名称不存在");
                 }
-                if (user.getU_Password().equals(loginForm.getPassword())) return Result.success();
+                idForm = new IdForm(0, user.getU_Id());
+                if (user.getU_Password().equals(loginForm.getPassword())) return Result.success(idForm);
                 else return Result.fail("wrong password");
             case 1 :
                 Admin admin = adminDao.findAdminByName(loginForm.getName());
                 if (admin == null) {
                     return Result.fail("该管理员名称不存在");
                 }
-                if (admin.getA_Password().equals(loginForm.getPassword())) return Result.success();
+                idForm = new IdForm(1, admin.getA_Id());
+                if (admin.getA_Password().equals(loginForm.getPassword())) return Result.success(idForm);
                 else return Result.fail("wrong password");
         }
         return Result.fail("登录运行错误");
